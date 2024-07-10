84c131afe67eb0210d033f00ebde2817cf34f115
Marco <maphy1707@gmail.com>
2024-07-10 16:59:39 -0600

    Dom - JQuery

---
+++ js/dom.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  //   $("p.para-1").css({ color: "red", background: "beige" });
+
+  //   $("p.para-2").addClass("miClase");
+  //   $("p.para-2").removeClass("miClase");
+
+  //   $("#btn-1").click(function () {
+  //     $("p.para-2").toggleClass("miClase");
+  //   });
+
+  //   //   $("#mi-div").text("Bienvenidos");
+  //   $("#mi-div").html("<h2>Curso de Jquery</h2>");
+
+  //   $("ul").append("<li>Item Agregado al Final</li>");
+  //   $("ul").prepend("<li>Item Agregado al Principio</li>");
+
+  //   $(".para-1").appendTo(".para-2");
+  //   $(".para-1").prependTo(".para-2");
+
+  //   $("ul").before("<h4>Hola</h4>");
+  //   $("ul").after("<h4>Mundo</h4>");
+
+  //   $("ul").empty();
+  //   $("ul").detach();
+
+  //   $("a").attr("target", "_blank");
+  //   //   alert($("a").attr("href"));
+  //   $("a").removeAttr("target");
+
+  $("p").wrapAll("<h3>");
+});
