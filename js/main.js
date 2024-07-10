7df4686134208be9c439fe1c2182d8d51b7aa4db
Marco <maphy1707@gmail.com>
2024-07-10 14:46:15 -0600

    Eventos de Mouse - Jquery

--- js/main.js
+++ js/main.js
@@ -1,27 +1,84 @@
 $(document).ready(function () {
-  // Selector por etiqueta
-  $("p").css("color", "blue");
-  $("p").css("font-size", "24px");
+  //   // Selector por etiqueta
+  //   $("p").css("color", "blue");
+  //   $("p").css("font-size", "24px");
+  //   // Selector por clase
+  //   $(".mi-clase").html("Texto cambiado");
+  //   // Selector por id
+  //   $("#mi-id").css("font-weight", "bold");
+  //   // Selector de descendencia
+  //   $("ul li").css("list-style", "none");
+  //   // Selector de descendencia especifico
+  //   $("#contenedor span").css("border", "1px solid red");
+  //   // Selector de primer elemento
+  //   $("ul li:first").css("background-color", "#eee");
+  //   // Selector de ultimo elemento
+  //   $("ul li:last").css("background-color", "#ccc");
+  //   // Selector elmento par o impar
+  //   $("tr:even").css("background-color", "#f0f0f0");
+  //   $("tr:odd").css("background-color", "#e0e0e0");
 
-  // Selector por clase
-  $(".mi-clase").html("Texto cambiado");
+  ////////////////////////////////////////////////////
+  // Eventos de Mouse
+  // Evento click
+  //   $("#btn-1").click(function () {
+  //     alert("Boton clickeado!!!");
+  //   });
 
-  // Selector por id
-  $("#mi-id").css("font-weight", "bold");
+  //   $("#btn-2").on("click", function () {
+  //     alert("Boton clickeado!!");
+  //   });
 
-  // Selector de descendencia
-  $("ul li").css("list-style", "none");
+  //   $("#btn-1").click(function () {
+  //     $(".para-1").hide();
+  //   });
 
-  // Selector de descendencia especifico
-  $("#contenedor span").css("border", "1px solid red");
+  //   $("#btn-2").click(function () {
+  //     $(".para-1").show();
+  //   });
 
-  // Selector de primer elemento
-  $("ul li:first").css("background-color", "#eee");
+  //   $("#btn-1").click(function () {
+  //     $(".para-1").toggle();
+  //   });
 
-  // Selector de ultimo elemento
-  $("ul li:last").css("background-color", "#ccc");
+  //   $("#btn-1").on("mouseenter", function () {
+  //     $(".para-1").toggle();
+  //   });
 
-  // Selector elmento par o impar
-  $("tr:even").css("background-color", "#f0f0f0");
-  $("tr:odd").css("background-color", "#e0e0e0");
+  //   $("#btn-1").on("mouseleave", function () {
+  //     $(".para-1").toggle();
+  //   });
+
+  //   $("#btn-1").hover(function () {
+  //     $(".para-1").toggle();
+  //   });
+
+  //   $("#btn-1").dblclick(function () {
+  //     $(".para-1").toggle();
+  //   });
+
+  //   $("#btn-1").mousemove(function () {
+  //     $(".para-1").toggle();
+  //   });
+
+  //   $("#btn-1").mousedown(function () {
+  //     $(".para-1").toggle();
+  //   });
+
+  //   $("#btn-1").mouseup(function () {
+  //     $(".para-1").toggle();
+  //   });
+
+  //   $("#btn-1").click(function (event) {
+  //     // console.log(event.currentTarget.id);
+  //     // console.log(event.currentTarget.innerHTML);
+  //     // console.log(event.currentTarget.outerHTML);
+  //     // console.log(event.currentTarget.className);
+  //     // alert(event.currentTarget.id);
+  //     console.log("Coordenadas Y: " + event.clientY + " X: " + event.clientX);
+  //   });
+
+  $(document).click(function (event) {
+    console.log("Coordenadas X: " + event.clientX + " Y: " + event.clientY);
+  });
 });
